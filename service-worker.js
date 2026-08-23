2d5021e35e5edc072cf07ff895fde975927ef267
ferrira19555 <ferreira1955520@gmail.com>
2026-08-23 08:15:45 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+'use strict';
+const CACHE='texto-rapido-v3';
+const FILES=['./','./index.html','./manifest.webmanifest','./icons/texto-180.png','./icons/texto-192.png','./icons/texto-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}))) });
